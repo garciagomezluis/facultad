08ec6f455963fd8aa648f3c39085baa155bb7dce
Luis García <garciagomezluis.94@gmail.com>
2015-11-30 03:52:08 -0300

    Continuo con diccRapido. Faltan tests.

--- aedii/tp3/modulos/diccRapido/diccRapido.h
+++ aedii/tp3/modulos/diccRapido/diccRapido.h
@@ -5,153 +5,150 @@
 
 using namespace std;
 using namespace aed2;
+#define TAM_TABLA_INI 10
+#define UMBRAL_FC   0.3
 
 template<typename A, typename B>
 class DiccRapido {
 
   public:
 
-  	DiccRapido();
+  	DiccRapido() {
+      dimension = TAM_TABLA_INI;
+      cantidad = 0;
+      tabla = Arreglo<Lista<Item> >(dimension);
 
-  	~DiccRapido();
+      for(int i = 0; i < dimension; i++) {
+        tabla.Definir(i, Lista<Item>());
+      }
+    }
 
-  	void Definir(const A& a, const B& b);
+  	void Definir(const A& a, const B& b) {
+      if(FactorDeCarga() > UMBRAL_FC) {
+        RedimensionarTabla(2 * dimension);
+      }
 
-  	bool Definido(const A& a) const;
+      typename Lista<Item>::Iterador it = tabla[FuncionDeHash(a)].CrearIt();
 
-  	const B& Significado(const A& a) const;
+      while(it.HaySiguiente() && it.Siguiente().clave != a) {
+        it.Avanzar();
+      }
+      cantidad += it.HaySiguiente() ? 0 : 1;
+      it.AgregarComoSiguiente(Item(a, b));
+    }
 
-    //Para tests
+  	bool Definido(const A& a) const {
+      typename Lista<Item>::const_Iterador it = tabla[FuncionDeHash(a)].CrearIt();
+      bool res = false;
+      while(it.HaySiguiente() && !res) {
+        res = it.Siguiente().clave == a;
+        it.Avanzar();
+      }
+      return res;
+    }
 
-    Nat CantClaves() const;
+    /**/
+  	const B& Significado(const A& a) const {
+      assert(Definido(a));
 
-    Conj<A> Claves() const;
+      typename Lista<Item>::const_Iterador it = tabla[FuncionDeHash(a)].CrearIt();
+      while(it.HaySiguiente()) {
+        if(it.Siguiente().clave == a) {
+          return it.Siguiente().valor;
+        }
+        it.Avanzar();
+      }
+    }
+    /**/
+
+    //Para tests ------------------------------------------------------------------------
+
+    Nat Total() const {
+      return cantidad;
+    }
+
+    Conj<A> Claves() const {
+      Conj<A> res;
+      for(int i = 0; i < dimension; i++) {
+        typename Lista<Item>::const_Iterador it = tabla[i].CrearIt();
+        while(it.HaySiguiente()) {
+          res.Agregar(it.Siguiente().clave);
+          it.Avanzar();
+        }
+      }
+
+      return res;
+    }
     
-    Nat colisiones() const;
+    Nat Colisiones() const {
+      Nat ret = 0;
+      for(int i=0; i < tabla.Tamanho(); i++){
+          ret = ret + (tabla[i].Longitud() == 0 ? 0 : tabla[i].Longitud() - 1);
+      }
+      return ret;
+    }
 
-    float FactorDeCarga() const;
+    float FactorDeCarga() const {
+      return (float)cantidad / (float)tabla.Tamanho();
+    }
+
+    ostream& mostrar(ostream& os) const {
+
+      os << "\n{\n";
+
+      for(int i = 0; i < dimension; i++) {
+        typename Lista<Item>::const_Iterador it = tabla[i].CrearIt();
+        while(it.HaySiguiente()) {
+          os << "\t" << it.Siguiente().clave << ": " << it.Siguiente().valor << "\n";
+          it.Avanzar();
+        }
+      }
+
+      os << "}\n";
+
+      return os;
+    }
 
   private:
 
-    #define TAM_TABLA_INI 100
-    #define UMBRAL_FC   0.75
-
   	struct Item {
-  		const A& clave;
-  		const B& valor;
+  		A clave;
+  		B valor;
 
-  		Item(const A& a, const B& b) : clave(a), valor(b) {}
+  		Item(const A a, const B b) : clave(a), valor(b) {}
   	};
 
   	Nat dimension;
-    Nat _cant_elems;
+    Nat cantidad;
   	Arreglo<Lista<Item> > tabla;
 
-    Nat FuncionHash(const A&) const;
+    Nat FuncionDeHash(const A& a) const {
+      return a % dimension;
+    }
 
-    void RedimensionarTabla(const Nat);
+    void RedimensionarTabla(const Nat d) {
+      Arreglo<Lista<Item> > tabla_vieja(tabla);
+      tabla = Arreglo<Lista<Item> >(d);
+      dimension = d;
+      cantidad = 0;
+      for(int i = 0; i < d; i++) {
+        tabla.Definir(i, Lista<Item>());
+      }
+
+      for(int i = 0; i < d; i++) {
+        typename Lista<Item>::const_Iterador it = tabla_vieja[i].CrearIt();
+        while(it.HaySiguiente()) {
+          Definir(it.Siguiente().clave, it.Siguiente().valor);
+          it.Avanzar();
+        }
+      }
+    }
 
 };
 
-DiccRapido::DiccRapido() {
-  dimension = TAM_TABLA_INI;
-  _cant_elems = 0;
-  tabla = Arreglo<Lista<Elemento> >(dimension);
-
-  for(int i = 0; i < dimension; i++) {
-    tabla[i] = Lista<Elemento>();
-  }
-}
-
-DiccRapido::~DiccRapido() {}
-
-void DiccRapido::Definir(const A& a, const B& b) {
-
-  if(FactorDeCarga() > UMBRAL_FC) {
-    RedimensionarTabla(2 * dimension);
-  }
-
-  //TODO
-    //incrementar _cant_elems!
-
-}
-
-bool DiccRapido::Definido(const A& a) const {
-  Lista<Item> candidatos = tabla[funcionDeHash(a)];
-  Lista<Item>::const_Iterador it = candidatos.CrearIt();
-  bool res = false;
-  while(it.HaySiguiente() && !res) {
-    res = it.Siguiente().clave == a;
-    it.Avanzar();
-  }
-  return res;
-}
-
-const B& DiccRapido::Significado(const A& a) const {
-  assert(Definido(a));
-
-  Lista<Item> candidatos = tabla[funcionDeHash(a)];
-  Lista<Item>::const_Iterador it = candidatos.CrearIt();
-  const B& res;
-  while(it.HaySiguiente()) {
-    if(it.Siguiente().clave == a) {
-      res = it.Siguiente().valor;
-    }
-    it.Avanzar();
-  }
-  return res;
-}
-
-void DiccRapido::RedimensionarTabla(const Nat d) {
-  Arreglo<Lista<Item> > tabla_vieja = tabla;
-  tabla = Arreglo<Lista<Item> >(d);
-  dimension = d;
-  _cant_elems = 0;
-  for(int i = 0; i < d; i++) {
-    tabla[i] = Lista<Elemento>();
-  }
-
-  for(int i = 0; i < d; i++) {
-    Lista<Item>::const_Iterador it = tabla_vieja[i].CrearIt();
-    while(it.HaySiguiente()) {
-      Definir(it.Siguiente().clave, it.Siguiente().valor);
-      it.Avanzar();
-    }
-  }
-}
-
-void DiccRapido::funcionDeHash(const A& a) const {
-  return a % dimension;
-}
-
-float DiccRapido::FactorDeCarga() const {
-  return float(_cant_elems)/ tabla.Tamanho();
-}
-
-Nat DiccRapido::CantClaves() const {
-  return _cant_elems;
-}
-
-Conj<A> DiccRapido::Claves() const {
-  Conj<A> res;
-
-  for(int i = 0; i < dimension; i++) {
-    Lista<Item>::const_Iterador it = tabla[i].CrearIt();
-    while(it.HaySiguiente()) {
-      res.Agregar(it.Siguiente());
-      it.Avanzar();
-    }
-  }
-
-  return res;
-}
-
-Nat DiccRapido::colisiones() const {
-  Nat ret = 0;
-  for(Nat i=0; i < tabla.Tamanho(); i++){
-      ret = ret + tabla[i].Longitud() - 1;
-  }
-  return ret;
+template<class A, class B>
+ostream& operator<<(ostream& out, const DiccRapido<A, B>& d) {
+  return d.mostrar(out);
 }
 
 #endif //DICCRAPIDO_H_
--- aedii/tp3/modulos/diccRapido/test.cpp
+++ aedii/tp3/modulos/diccRapido/test.cpp
@@ -23,18 +23,39 @@ string remove_spaces(const string& s) {
 }
 
 void diccRapido_constructor() {
-	DiccRapido<int, int> d();
+	DiccRapido<int, int> d;
 }
 
 void diccRapido_definir() {
 	DiccRapido<int, int> d;
 	d.Definir(1, 1);
+	ASSERT_EQ(d.Total(), 1);
+	ASSERT_EQ(remove_spaces(to_s(d.Claves())), remove_spaces(to_s("{ 1 }")));
+	ASSERT_EQ(d.Colisiones(), 0);
+	d.Definir(2, 1);
+	ASSERT_EQ(d.Total(), 2);
+	ASSERT_EQ(remove_spaces(to_s(d.Claves())), remove_spaces(to_s("{ 2, 1 }")));
+	ASSERT_EQ(d.Colisiones(), 0);
+	d.Definir(1, 3);
+	ASSERT_EQ(d.Total(), 2);
+	ASSERT_EQ(remove_spaces(to_s(d.Claves())), remove_spaces(to_s("{ 2, 1 }")));
+	ASSERT_EQ(d.Colisiones(), 1);
+	d.Definir(102, 5);
+	ASSERT_EQ(d.Total(), 3);
+	ASSERT_EQ(remove_spaces(to_s(d.Claves())), remove_spaces(to_s("{ 102, 2, 1 }")));
+	ASSERT_EQ(d.Colisiones(), 2);
+	d.Definir(103, 3);
 }
 
 int main() {
 	//test interfáz pública de campus
 	RUN_TEST(diccRapido_constructor);
 	RUN_TEST(diccRapido_definir);
+	//RUN_TEST(diccRapido_definido);
+	//RUN_TEST(diccRapido_significado);
+	//RUN_TEST(diccRapido_total);
+	//RUN_TEST(diccRapido_claves);
+	//RUN_TEST(diccRapido_colisiones);
 
 	return 0;
 }
