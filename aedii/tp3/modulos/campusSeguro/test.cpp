615b1510474832547812b34f3678e141e8d1dd65
julifgo <juli?fgo@hotmail.com>
2015-11-30 17:57:51 -0300

    Agregar makefile y estructura de test a campusSeguro

---
+++ aedii/tp3/modulos/campusSeguro/test.cpp
@@ -0,0 +1,29 @@
+#include <string>
+#include <iostream>
+
+#include "campusSeguro.h"
+
+using namespace aed2;
+using namespace std;
+
+void Inicializacion()
+{
+
+	
+	
+	
+	ASSERT_EQ(true,true);
+	
+    
+
+}
+
+
+
+int main(int argc, char **argv)
+{
+
+	RUN_TEST(Inicializacion);
+	
+	return 0;
+}
